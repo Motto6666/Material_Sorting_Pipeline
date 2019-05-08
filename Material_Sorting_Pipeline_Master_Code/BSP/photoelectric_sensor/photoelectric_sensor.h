1461b854f1e21be40a269ac195072eb524f05e40
=Motto6666 <=790168665@qq.com>
2019-05-08 18:07:11 +0800

    修改了光电传感器的触发引脚（修改为PC2引脚），测试成功

--- Material_Sorting_Pipeline_Master_Code/BSP/photoelectric_sensor/photoelectric_sensor.h
+++ Material_Sorting_Pipeline_Master_Code/BSP/photoelectric_sensor/photoelectric_sensor.h
@@ -3,14 +3,14 @@
 
 #include "stm32f10x.h"
 
-#define SENSOR_PORT_CLOCK        				RCC_APB2Periph_GPIOA
-#define SENSOR_PORT              				GPIOA
-#define SENSOR_PIN                  		GPIO_Pin_1
-#define SENSOR_PORTSOURCE   						GPIO_PortSourceGPIOA
-#define SENSOR_EXTI_GPIO_PINSOURCE  		GPIO_PinSource1
-#define SENSOR_EXTI_LINE            		EXTI_Line1
-#define SENSOR_EXTI_IRQn            		EXTI1_IRQn
-#define SENSOR_EXTI_IRQHandler      		EXTI1_IRQHandler
+#define SENSOR_PORT_CLOCK        				RCC_APB2Periph_GPIOC
+#define SENSOR_PORT              				GPIOC
+#define SENSOR_PIN                  		GPIO_Pin_2
+#define SENSOR_PORTSOURCE   						GPIO_PortSourceGPIOC
+#define SENSOR_EXTI_GPIO_PINSOURCE  		GPIO_PinSource2
+#define SENSOR_EXTI_LINE            		EXTI_Line2
+#define SENSOR_EXTI_IRQn            		EXTI2_IRQn
+#define SENSOR_EXTI_IRQHandler      		EXTI2_IRQHandler
 
 #define PHOTOELECTRIC_SENSOR_ENABLE			{ EXTI->IMR |= SENSOR_EXTI_LINE; Check_Object = 0;} 
 #define PHOTOELECTRIC_SENSOR_DISABLE		{ EXTI->IMR &= ~(SENSOR_EXTI_LINE); Check_Object = 0;}  
